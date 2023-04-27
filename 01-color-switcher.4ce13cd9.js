c8088d6072fba23e0197af12997034b2e26f1754
Nefilim47 <65806868+Nefilim47@users.noreply.github.com>
2023-04-27 19:43:45 +0000

    Deploying to gh-pages from @ Nefilim47/goit-js-hw-09@70a8eb2a3477ce0b05e09e009f7670adb60c8766 🚀

---
+++ 01-color-switcher.4ce13cd9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let r;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(r)}));
+//# sourceMappingURL=01-color-switcher.4ce13cd9.js.map
